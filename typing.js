c0d42a1ff9d7f41de2747afad2fd6549b742c23e
xuzhipeng <595187998@qq.com>
2020-11-07 00:32:42 +0800

    Feature: finish all the basic functions

--- typing.js
+++ typing.js
@@ -11,27 +11,74 @@ var count = 0;//纪录对的字母的个数
 var startTime = 0;//纪录开始时间
 var tempTime = 0;//记录每个单词的速度
 var timeouttime = 0;//记录暂停的时间
+var sigtime = 0;
+var jugall = 0;
+var jugrig = 0;
+var temp = 0;
+var offf1 = 0;
+var flag = 0;
+var linei = 0;
+var fixtail = 0;
+var blankspace = new Array();
+var fix = new Array();
+fix[fix.length]=3;fix[fix.length]=4;fix[fix.length]=5;fix[fix.length]=10;fix[fix.length]=7;
+var blankspacetail = 0;
+blankspace[0] = 6;blankspace[1] = 5;blankspace[2] = 4;blankspace[3] = 4;blankspace[4] = 2;
+var right = "./right.mp3";
+var wrong = "./wrong.mp3";
+var rightsound = new Audio(right);
+var wrongsound = new Audio(wrong);
 var fx2 = function(e) {
     if(words[i].innerHTML == String.fromCharCode(e.keyCode.toString()).toLowerCase())//正确变色
     {
         words[i].style.backgroundColor = 'rgba(0,255,0,.2)';
         words[i].style.color = 'green';
-        i++;
         count++;
+        jugall++;
+        jugrig++;
     }
     else {//错误变色
         words[i].style.backgroundColor = 'rgba(255,0,0,.2)';
         words[i].style.color = 'red';
-        i++;
+        jugall++;
+        wrongsound.play();
     }
     tempTime = +new Date();
+    i++;linei++;
     accuracy.innerHTML = ((count / i) * 100).toFixed(2) + '%' + ' ';//准确额度计算
     speed.innerHTML = (i / ((tempTime - startTime) / 60 / 60)).toFixed(2) + 'wpm'//速度计算
     progress.style.width = progress.clientWidth + 5 + "px";
+    if(words[i-1].innerHTML == ' ')
+    {
+        if(jugrig == jugall) rightsound.play();
+        if(blankspacetail >= blankspace[offf1]) {offf1++;blankspacetail=0;linei = fix[fixtail++];}
+        startButton.style.display = 'none';
+        temp = +new Date()
+        var jug = document.createElement('div');
+        jug.innerHTML = (jugall / ((temp - sigtime) / 60 / 60)).toFixed(2);
+        text.appendChild(jug);
+        jug.style.position = 'absolute';
+        jugall = 0
+        jugrig = 0;
+        sigtime = +new Date();
+        temp = 0;
+        blankspacetail++;
+        console.log(offf1);
+        console.log(blankspace[offf1]);
+        console.log(blankspacetail);
+        jug.style.left = (Math.max(linei,1)+1) * 15+ 'px';
+        jug.style.top = 10 + 50*offf1 + 'px';
+    }
+    if(i==words.length) {
+        alert('congratulations');
+        if((count / i)>0.8 && (i / ((tempTime - startTime) / 60 / 60))>10)
+        alert('five stars');
+    }
 }
 
 var fx1 = function() {//开始按钮的函数
     startTime = +new Date();
+    sigtime  = +new Date();
     console.log('开始的时间是'+startTime+'');
     console.log('执行fx1');
     document.addEventListener('keydown',fx2);
@@ -60,7 +107,7 @@ var fx4 = function() {//变量清空
         words[temp].style.color = '#757575';
     }
     document.removeEventListener('keydown',fx2);//取消键盘事件
-    i = 0; count = 0; startTime = 0;tempTime = 0;timeouttime = 0;//变量初始化
+    i = 0; count = 0; startTime = 0;tempTime = 0;timeouttime = 0;jugall = 0; jugrig = 0;
     progress.style.width = 5 +'px';
     speed.innerHTML = '0wpx';
     accuracy.innerHTML = '0%';
